a4e5457d19273869cf7f0bfeb6f311949e617c27
Gabriel Serejo <gabriel_serejo@live.com>
2024-05-17 09:33:26 -0300

    Remove GraphiQL

--- Server/Types/Data.cs
+++
@@ -1,9 +0,0 @@
-namespace Server;
-
-public class Data
-{
-    public List<Thing> Things { get; } = new List<Thing>
-    {
-        new Thing("1", "Name")
-    };
-}
--- Server/Types/Query.cs
+++
@@ -1,7 +0,0 @@
-namespace Server;
-
-public class Query
-{
-    public Thing? Thing([ID] string id, Data repository)
-        => repository.Things.FirstOrDefault(t => t.Id.Equals(id));
-}
